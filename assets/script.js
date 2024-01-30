9d2f10de4e074d0cd11c729aa3ffecc5ef5ff821
adahollins <adrianna.hollins@hotmail.com>
2024-01-30 14:09:01 +0000

    Got API working

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,45 @@
 $(document).ready(function () {
     // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}
     
-    var apiKey = config.WEATHER_KEY;
-    var queryURLWeather = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon +"&appid=" + apiKey;
-    var queryURLConverter = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
-
-    var lat = queryURLConverter;
-    var lon = queryURLConverter;
+    var apiKey = WEATHER_KEY;
 
     var searchString = "";
-
-    var cityName = "";
 
     var todayHTML = $("#today");
     var forecastHTML = $("#forecast");
 
+// Convert lat and lon to location
 
+$("#search-button").on("click", function(event) {
+    event.preventDefault();
+
+    $("#today").empty();
+    searchString = $("#search-input").val();
+    queryURLConverter = ("https://api.openweathermap.org/data/2.5/weather?q=" + searchString + "&limit=1&appid=" + apiKey);
+
+
+    fetch(queryURLConverter)
+        .then(function (response) {
+        return response.json();
+        })
+    .then(function (geo) {
+        var lat = geo.coord.lat;
+        var lon = geo.coord.lon;
+
+        var queryURLWeather = ("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon +"&limit=1&appid=" + apiKey + "&units=metric");
+
+        fetch(queryURLWeather)
+            .then(function  (response) {
+            return response.json();
+            })
+        .then(function (data) {
+
+            var cityName = data.city.name;
+
+            $("#today").text(cityName);
+        });
+        });
+
+
+});
 });
